e51772d12e34b5d2c951d27e88869282de2e36d1
robynae <92494880+robynae@users.noreply.github.com>
2023-12-13 15:44:58 -0600

    Delete boxes module

--- script.js
+++ script.js
@@ -3,28 +3,19 @@ const gameBoard = (function () {
     let columns = 3;
     let board = [];
 
-    const makeBoard = function () {
         for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(boxes)
+            board[i].push('')
         }
     }
-    console.log(board);
-}
-return {makeBoard};
+
+
+    const getBoard = () => board;
+
+return {getBoard};
 })();
 
-const boxes = (function () {
-  value = ''
-
-  const changeBoxValue = function(player) {
-    value = player
-  }
-
-  const getValue = () => value;
-  return {changeBoxValue, getValue}
-})();
 
 const gameController = (function() {
   const players = [
